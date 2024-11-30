9a88c8318e2ccdab56b74362533d602f288cfdbb
Leo G <68085263+Mythilllian@users.noreply.github.com>
2024-11-30 16:28:27 -0500

    Update Minimap.gd

--- Minimap.gd
+++ Minimap.gd
@@ -1,39 +1,35 @@
 extends Control
 
+@export var camera_node: NodePath
+@export var tilemap_nodes: Array[NodePath]
+@export var cell_colors: Dictionary
+@export var zoom: float = 1.0
 
-export(NodePath) var camera_node
-export(Array, NodePath) var tilemap_nodes
-export(Dictionary) var cell_colors
-export var zoom = 1.0
-
-
-onready var camera  = get_node(camera_node)
-var tilemaps = []
-
+@onready var camera: Camera2D = get_node(camera_node)
+var tilemaps: Array[TileMap] = []
 
 func _ready():
-    for node in tilemap_nodes:
-        tilemaps.append(get_node(node))
+	for node_path in tilemap_nodes:
+		var tilemap: TileMap = get_node(node_path)
+		tilemaps.append(tilemap)
 
-
-func get_cells(tilemap : TileMap, id):
-    return tilemap.get_used_cells_by_id(id)
-
+func get_cells(tilemap: TileMap, id: int) -> Array[Vector2i]:
+	return tilemap.get_used_cells_by_tileset_id(id)
 
 func _draw():
-    draw_set_transform(rect_size / 2, 0, Vector2.ONE)
+	draw_set_transform_matrix(Transform2D(0, size / 2))
+	
+	for tilemap in tilemaps:
+		var camera_position = camera.get_screen_center()
+		var camera_cell = tilemap.local_to_map(camera_position)
+		var tilemap_offset = camera_cell + (camera_position - tilemap.map_to_local(camera_cell)) / tilemap.cell_size
 
-    for tilemap in tilemaps:
-        var camera_position = camera.get_camera_screen_center()
-        var camera_cell = tilemap.world_to_map(camera_position)
-        var tilemap_offset = camera_cell + (camera_position - tilemap.map_to_world(camera_cell)) / tilemap.cell_size
+		for id in cell_colors.keys():
+			var color: Color = cell_colors[id]
+			var cells = get_cells(tilemap, id)
+			for cell in cells:
+				var position = (cell - tilemap_offset) * zoom
+				draw_rect(Rect2(position, Vector2.ONE * zoom), color)
 
-        for id in cell_colors.keys():
-            var color = cell_colors[id]
-            var cells = get_cells(tilemap, id)
-            for cell in cells:
-                draw_rect(Rect2((cell - tilemap_offset) * zoom, Vector2.ONE * zoom), color)
-
-
-func _process(delta):
-    update()
+func _process(_delta):
+	queue_redraw() 
